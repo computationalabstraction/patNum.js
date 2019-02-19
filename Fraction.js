94aa9eceb6eb0134a1e6572b1e89d61b8fec21a9
Archan Patkar <pat@archanpatkar.com>
2019-02-20 03:34:02 +0530

    Update

--- Fraction.js
+++ Fraction.js
@@ -9,12 +9,54 @@ function lcm(a,b)
     return a / gcd(a,b) * b
 }
 
+function decToInt(n)
+{
+    return parseInt((""+n).split(".").reduce((acc,v) => acc+v));
+}
+
+function digitsAfterDot(n)
+{
+    return (""+n).split(".")[1].length;
+}
+
+function digits(n)
+{
+    if(n == 0) return 0;
+    return 1 + digits(n/10);
+}
+
 class Fraction
 {
-    constructor(n,d)
+    constructor(n,d=1)
     {
-        this.n = n;
-        this.d = d;
+        let flag1 = false;
+        let flag2 = false;
+        if(!Number.isInteger(n))
+        {
+            flag1 = true;
+            this.n = decToInt(n);
+        }
+        else this.n = n;
+        if(!Number.isInteger(d))
+        {
+            flag2 = true;
+            this.n = decToInt(n);
+        }
+        else this.d = d;
+        if(flag1 || flag2)
+        {
+            if(flag1)
+            {
+                this.d *= 10**digitsAfterDot(n);
+            }
+            if(flag2)
+            {
+                this.n *= 10**digitsAfterDot(d);
+            }
+            const temp = this.reduce();
+            this.n = temp.n;
+            this.d = temp.d;
+        }
     }
 
     add(x)
@@ -23,8 +65,8 @@ class Fraction
         {
             x = new Fraction(x,1);
         }
-        else if(x instanceof Fraction)
-         {
+        if(x instanceof Fraction)
+        {
             let temp = new Fraction();
             if(this.d == x.d) 
             {
@@ -46,7 +88,7 @@ class Fraction
         {
             x = new Fraction(x,1);
         }
-        else if(x instanceof Fraction)
+        if(x instanceof Fraction)
         {
             let temp = new Fraction();
             if(this.d == x.d) 
@@ -69,7 +111,7 @@ class Fraction
         {
             x = new Fraction(x,1);
         }
-        else if(x instanceof Fraction)
+        if(x instanceof Fraction)
         {
             let temp = new Fraction();
             temp.n = (this.n * x.n);
@@ -84,11 +126,29 @@ class Fraction
         {
             x = new Fraction(1,x);
         }
-        else if(x instanceof Fraction)
+        if(x instanceof Fraction)
         {
             x = new Fraction(x.d,x.n);
             return this.multiply(x);
         }
+    }
+
+    reduce()
+    {
+        const gcf = gcd(this.n,this.d);
+        this.n /= gcf;
+        this.d /= gcf;
+        return this;
+    }
+
+    gcf()
+    {
+        return gcd(this.n,this.d);
+    }
+
+    toDecimal()
+    {
+        return this.n/this.d;
     }
 
     toString()
@@ -97,18 +157,47 @@ class Fraction
     }
 }
 
+function recurringToFraction(bd,nrd,rd)
+{
+    let whole;
+    let wholenr;
+    let nd;
+    let ndr;
+    if(nrd != undefined)
+    {
+        whole = parseInt("" + bd + nrd + rd);
+        wholenr = parseInt("" + bd + nrd);
+        nd = 10**(("" + nrd + rd).length);
+        ndr = 10**(("" + nrd).length);
+    }
+    else 
+    {   
+        whole = parseInt("" + bd + rd);
+        wholenr = parseInt("" + bd);
+        nd = 10**(("" + rd).length);
+        ndr = 10**0;
+    }
+    return new Fraction(whole-wholenr,nd-ndr);
+}
 
-let f1 = new Fraction(5,2);
-let f2 = new Fraction(7,3);
-
+let f1 = new Fraction(0.75);
 console.log(f1.toString());
+
+let f2 = recurringToFraction(0,undefined,3);
 console.log(f2.toString());
-console.log(f1.add(f2).toString());
+
+// let f1 = new Fraction(5,2);
+// let f2 = new Fraction(7,3);
+
+// console.log(f1.toString());
+// console.log(f2.toString());
+// console.log(f1.add(f2).toString());
 
 
-f1 = new Fraction(4,6);
-f2 = new Fraction(2,5);
+// f1 = new Fraction(4,6);
+// f2 = new Fraction(2,5);
 
-console.log(f1.toString());
-console.log(f2.toString());
-console.log(f1.substract(f2).toString());
+// console.log(f1.toString());
+// console.log(f1.reduce().toString());
+// console.log(f2.toString());
+// console.log(f1.substract(f2).toString());
