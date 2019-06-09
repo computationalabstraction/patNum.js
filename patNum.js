c0d0010c50c758feb5ea1d2c4fe7aa92ea69f676
archanpatkar <pat@archanpatkar.com>
2019-06-10 00:32:18 +0530

    Update

--- patNum.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ...require("./Stats"),
-    ...require("./Matrix"),
-    ...require("./Complex"),
-    ...require("./Waves"),
-    ...require("./Point"),
-    ...require("./Fraction"),
-    ...require("./Polynomial")
-}
